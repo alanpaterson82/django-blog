62c3052720cde27bc194feceb3d9a3b03e93e089
alanpaterson82 <>
2024-01-15 19:56:19 +0000

    create the project and blog app challenge

--- codestar/urls.py
+++ codestar/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from blog import views as blog_views
 
 urlpatterns = [
+    path("blog/", blog_views.my_blog, name='blog'),
     path('admin/', admin.site.urls),
 ]
