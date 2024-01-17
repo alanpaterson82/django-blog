a7f837b8d27f4b1933f08472a1d34dabc2a87bd5
alanpaterson82 <>
2024-01-17 21:14:00 +0000

    build the post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-alanpaterson82\
-    -django-bl-g3i3zejy1h.us2.codeanyapp.com', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-\
+    alanpaterson82-django-bl-g3i3zejy1h.us2.codeanyapp.com', '.herokuapp.com']
 
 
 # Application definition
@@ -90,22 +90,31 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com"
+]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password\
+            _validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password\
+            _validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password\
+            _validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password\
+            _validation.NumericPasswordValidator',
     },
 ]
 
