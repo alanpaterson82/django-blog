e16cffd605d1dfe4835b5781c6942ba3baaba9ec
alanpaterson82 <>
2024-01-19 10:58:29 +0000

    migrations

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-alanpaterson82-django-bl-g3i3zejy1h.us2.codeanyapp.com', '.herokuapp.com']
 
