a186b656e9c5bffac388a43222d17f8e929e276e
alanpaterson82 <>
2024-01-27 14:47:26 +0000

    create the about app

--- codestar/urls.py
+++ codestar/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
+    path("about/", include("about.urls"), name="about-urls"),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path("", include("blog.urls"), name="blog-urls"),
