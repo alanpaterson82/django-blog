a0d6a414b50328d8fe9e06ae6c303f0d95e47f30
alanpaterson82 <>
2024-01-28 16:22:57 +0000

    end of tutorial

--- blog/views.py
+++ blog/views.py
@@ -22,7 +22,12 @@ def post_detail(request, slug):
 
     ``post``
         An instance of :model:`blog.Post`.
-
+    ``comments``
+        All approved comments related to the post.
+    ``comment_count``
+        A count of approved comments related to the post.
+    ``comment form``
+        An instance of :form:`blog.CommentForm`.
     **Template:**
 
     :template:`blog/post_detail.html`
